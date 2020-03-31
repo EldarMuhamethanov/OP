e55e6905d146f23667079fc9f3f9f191cc35c990
EldarMuhamethanov <eldarmy@mail.ru>
2020-03-31 11:55:59 +0300

    FIX

--- lw18/1.pas
+++ lw18/1.pas
@@ -9,36 +9,52 @@ VAR
   Student: 1 .. ClassSize;
   NextScore: Score;
   Ave, TotalScore, ClassTotal: INTEGER;
+  Error: BOOLEAN;
 BEGIN {AverageScore}
   ClassTotal := 0;
   WRITELN('Student averages:');
   Student := 1;
-  WHILE Student < ClassSize + 1
+  Error := FALSE;
+  WHILE (Student < ClassSize + 1) AND NOT(Error)
   DO 
     BEGIN
       TotalScore := 0;
       WhichScore := 1;
-      WHILE WhichScore < NumberOfScores + 1
+      WHILE (WhichScore < NumberOfScores + 1) AND NOT(Error)
       DO
         BEGIN
-          READ(NextScore);
+          IF NOT EOLN
+          THEN
+            READ(NextScore)
+          ELSE
+            Error := TRUE;
           TotalScore := TotalScore + NextScore;
-          WhichScore := WhichScore + 1;
+          WhichScore := WhichScore + 1
         END;
       READLN;
-      TotalScore := TotalScore * 10;
-      Ave := TotalScore DIV NumberOfScores;
-      WRITE('average: ');
-      IF Ave MOD 10 >= 5
+      IF NOT(Error)
       THEN
-        WRITELN(Ave DIV 10 + 1)
-      ELSE
-        WRITELN(Ave DIV 10);
-      ClassTotal := ClassTotal + TotalScore;
-      Student := Student + 1;
+        BEGIN
+          TotalScore := TotalScore * 10;
+          Ave := TotalScore DIV NumberOfScores;
+          WRITE('average: ');
+          IF Ave MOD 10 >= 5
+          THEN
+            WRITELN(Ave DIV 10 + 1)
+          ELSE
+          WRITELN(Ave DIV 10);
+          ClassTotal := ClassTotal + TotalScore;
+          Student := Student + 1
+        END
     END;
   WRITELN;
-  WRITELN ('Class average:');
-  ClassTotal := ClassTotal DIV (ClassSize * NumberOfScores);
-  WRITELN(ClassTotal DIV 10, '.', ClassTotal MOD 10:1)
+  IF NOT(Error)
+  THEN
+    BEGIN
+      WRITELN ('Class average:');
+      ClassTotal := ClassTotal DIV (ClassSize * NumberOfScores);
+      WRITELN(ClassTotal DIV 10, '.', ClassTotal MOD 10:1)
+    END
+  ELSE
+    WRITELN('ньхайю ббндю!!!')
 END.  {AverageScore}
--- lw18/2.pas
+++ lw18/2.pas
@@ -11,11 +11,12 @@ VAR
   Ave, TotalScore, ClassTotal: INTEGER;
   Ch: CHAR;
   StudName: TEXT;
+  Error: BOOLEAN;
 BEGIN {AverageScore}
   ClassTotal := 0;
   WRITELN('Student averages:');
   Student := 1;
-  WHILE Student < ClassSize + 1
+  WHILE (Student < ClassSize + 1) AND NOT(Error)
   DO 
     BEGIN
       TotalScore := 0;
@@ -29,34 +30,48 @@ BEGIN {AverageScore}
           READ(INPUT, Ch)          
         END;
       WRITELN(StudName);
-      WHILE WhichScore < NumberOfScores + 1
+      WHILE (WhichScore < NumberOfScores + 1) AND NOT(Error)
       DO
         BEGIN
-          READ(INPUT, NextScore);
+          IF NOT EOLN
+          THEN
+            READ(NextScore)
+          ELSE
+            Error := TRUE;
           TotalScore := TotalScore + NextScore;
-          WhichScore := WhichScore + 1;
+          WhichScore := WhichScore + 1
         END;
       READLN(INPUT);
-      TotalScore := TotalScore * 10;
-      Ave := TotalScore DIV NumberOfScores;
-      RESET(StudName);
-      WHILE (NOT EOLN(StudName))
-      DO
-        BEGIN
-          READ(StudName, Ch);
-          WRITE(OUTPUT, Ch)
-        END;
-      WRITE(OUTPUT, ' average: ');
-      IF Ave MOD 10 >= 5
+      IF NOT(Error)
       THEN
-        WRITELN(OUTPUT, Ave DIV 10 + 1)
-      ELSE
-        WRITELN(OUTPUT, Ave DIV 10);
-      ClassTotal := ClassTotal + TotalScore;
-      Student := Student + 1;
+        BEGIN
+          TotalScore := TotalScore * 10;
+          Ave := TotalScore DIV NumberOfScores;
+          RESET(StudName);
+          WHILE (NOT EOLN(StudName))
+          DO
+            BEGIN
+              READ(StudName, Ch);
+              WRITE(OUTPUT, Ch)
+            END;
+          WRITE(OUTPUT, ' average: ');
+          IF Ave MOD 10 >= 5
+          THEN
+            WRITELN(OUTPUT, Ave DIV 10 + 1)
+          ELSE
+            WRITELN(OUTPUT, Ave DIV 10);
+          ClassTotal := ClassTotal + TotalScore;
+          Student := Student + 1
+        END
     END;
   WRITELN(OUTPUT);
-  WRITELN (OUTPUT, 'Class average:');
-  ClassTotal := ClassTotal DIV (ClassSize * NumberOfScores);
-  WRITELN(OUTPUT, ClassTotal DIV 10, '.', ClassTotal MOD 10:1)
+  IF NOT(Error)
+  THEN
+    BEGIN
+      WRITELN ('Class average:');
+      ClassTotal := ClassTotal DIV (ClassSize * NumberOfScores);
+      WRITELN(ClassTotal DIV 10, '.', ClassTotal MOD 10:1)
+    END
+  ELSE
+    WRITELN('ньхайю ббндю!!!')
 END.  {AverageScore}
