304d8ea155f3f7c2d2e562bcea8f205efa5fb1e7
EldarMuhamethanov <eldarmy@mail.ru>
2020-03-31 12:11:29 +0300

    FIX

--- Lw17/1.pas
+++ Lw17/1.pas
@@ -13,36 +13,16 @@ BEGIN{ReadDigit}
       IF (Ch >= '0') AND (Ch <= '9')
       THEN
         BEGIN
-          IF (Ch = '0')
-          THEN
-            D := 0;
-          IF (Ch = '1')
-          THEN
-            D := 1; 
-          IF (Ch = '2')
-          THEN
-            D := 2;
-          IF (Ch = '3')
-          THEN
-            D := 3;
-          IF (Ch = '4')
-          THEN
-            D := 4;
-          IF (Ch = '5')
-          THEN
-            D := 5;
-          IF (Ch = '6')
-          THEN
-            D := 6;
-          IF (Ch = '7')
-          THEN
-            D := 7;
-          IF (Ch = '8')
-          THEN
-            D := 8;
-          IF (Ch = '9')
-          THEN
-            D := 9              
+          IF (Ch = '1') THEN D := 1 ELSE
+          IF (Ch = '2') THEN D := 2 ELSE
+          IF (Ch = '3') THEN D := 3 ELSE
+          IF (Ch = '4') THEN D := 4 ELSE
+          IF (Ch = '5') THEN D := 5 ELSE
+          IF (Ch = '6') THEN D := 6 ELSE
+          IF (Ch = '7') THEN D := 7 ELSE
+          IF (Ch = '8') THEN D := 8 ELSE
+          IF (Ch = '9') THEN D := 9 ELSE
+          IF (Ch = '0') THEN D := 0 
         END
     END
 END;{ReadDigit}
