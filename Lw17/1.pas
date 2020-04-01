8ba2b9a2742bb71aeb254de932097834df4050f1
EldarMuhamethanov <eldarmy@mail.ru>
2020-04-01 12:22:14 +0300

    fix

--- Lw17/1.pas
+++ Lw17/1.pas
@@ -10,20 +10,16 @@ BEGIN{ReadDigit}
   THEN
     BEGIN
       READ(F, Ch); 
-      IF (Ch >= '0') AND (Ch <= '9')
-      THEN
-        BEGIN
-          IF (Ch = '1') THEN D := 1 ELSE
-          IF (Ch = '2') THEN D := 2 ELSE
-          IF (Ch = '3') THEN D := 3 ELSE
-          IF (Ch = '4') THEN D := 4 ELSE
-          IF (Ch = '5') THEN D := 5 ELSE
-          IF (Ch = '6') THEN D := 6 ELSE
-          IF (Ch = '7') THEN D := 7 ELSE
-          IF (Ch = '8') THEN D := 8 ELSE
-          IF (Ch = '9') THEN D := 9 ELSE
-          IF (Ch = '0') THEN D := 0 
-        END
+      IF (Ch = '1') THEN D := 1 ELSE
+      IF (Ch = '2') THEN D := 2 ELSE
+      IF (Ch = '3') THEN D := 3 ELSE
+      IF (Ch = '4') THEN D := 4 ELSE
+      IF (Ch = '5') THEN D := 5 ELSE
+      IF (Ch = '6') THEN D := 6 ELSE
+      IF (Ch = '7') THEN D := 7 ELSE
+      IF (Ch = '8') THEN D := 8 ELSE
+      IF (Ch = '9') THEN D := 9 ELSE
+      IF (Ch = '0') THEN D := 0 
     END
 END;{ReadDigit}
 BEGIN
