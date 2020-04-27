c933077dddc98e01b6e09293addb45141d27b78f
EldarMuhamethanov <eldarmy@mail.ru>
2020-04-27 14:52:19 +0300

    LW 18/2 FIX

--- lw18/2.pas
+++ lw18/2.pas
@@ -1,77 +1,79 @@
 PROGRAM AverageScore(INPUT, OUTPUT);
 CONST
+  Min = 0;
+  Max = 100;
   NumberOfScores = 4;
   ClassSize = 4;
 TYPE
   Score = 0 .. 100;
 VAR
-  WhichScore: 1 .. NumberOfScores;
-  Student: 1 .. ClassSize;
+  WhichScore: 0 .. NumberOfScores;
+  Student: 0 .. ClassSize;
   NextScore: Score;
   Ave, TotalScore, ClassTotal: INTEGER;
+  Error, Overflow: BOOLEAN;
+PROCEDURE ReadName(VAR FIn, Fout: TEXT); {CopyWord}
+VAR
   Ch: CHAR;
-  StudName: TEXT;
-  Error: BOOLEAN;
+BEGIN
+  WHILE ((NOT EOLN(FIn)) AND (Ch <> ' '))
+  DO
+    BEGIN
+      READ(FIn, Ch);
+      WRITE(Fout, Ch)
+    END
+END; {CopyWord}
 BEGIN {AverageScore}
+  Overflow := FALSE;
+  Error := FALSE;
   ClassTotal := 0;
+  Student := 0;
   WRITELN('Student averages:');
-  Student := 1;
-  WHILE (Student < ClassSize + 1) AND NOT(Error)
+  WHILE ((Student < ClassSize) AND NOT EOLN AND NOT Overflow AND NOT Error)
   DO 
     BEGIN
+      ReadName(INPUT, OUTPUT);
+      Student := Student + 1;
       TotalScore := 0;
-      WhichScore := 1;
-      REWRITE(StudName);
-      READ(INPUT, Ch);
-      WHILE (Ch <> ' ')
+      WhichScore := 0;
+      WHILE ((WhichScore < NumberOfScores) AND NOT Overflow AND NOT EOLN)
       DO
         BEGIN
-          WRITE(StudName, Ch);
-          READ(INPUT, Ch)          
-        END;
-      WRITELN(StudName);
-      WHILE (WhichScore < NumberOfScores + 1) AND NOT(Error)
-      DO
-        BEGIN
-          IF NOT EOLN
-          THEN
-            READ(NextScore)
+          READ(NextScore);
+          Overflow := ((Max < NextScore) OR (NextScore < Min ));
+          IF NOT Overflow
+          THEN                                                       
+            BEGIN
+              WhichScore := WhichScore + 1;
+              TotalScore := TotalScore + NextScore
+            END
           ELSE
-            Error := TRUE;
-          TotalScore := TotalScore + NextScore;
-          WhichScore := WhichScore + 1
+            WRITE('ICORRECT RATING ENTERED')
         END;
-      READLN(INPUT);
-      IF NOT(Error)
+      Error := NOT(EOLN AND (WhichScore = NumberOfScores)) AND NOT Overflow;
+      IF Error
+      THEN
+        WRITE('ICORRECT NUMBER OF RATINGS ENTERED');
+      IF (NOT Overflow AND NOT Error)
       THEN
         BEGIN
+          READLN;
           TotalScore := TotalScore * 10;
           Ave := TotalScore DIV NumberOfScores;
-          RESET(StudName);
-          WHILE (NOT EOLN(StudName))
-          DO
-            BEGIN
-              READ(StudName, Ch);
-              WRITE(OUTPUT, Ch)
-            END;
-          WRITE(OUTPUT, ' average: ');
-          IF Ave MOD 10 >= 5
+          IF (Ave MOD 10 >= 5)
           THEN
-            WRITELN(OUTPUT, Ave DIV 10 + 1)
+            WRITELN(Ave DIV 10 + 1)
           ELSE
-            WRITELN(OUTPUT, Ave DIV 10);
-          ClassTotal := ClassTotal + TotalScore;
-          Student := Student + 1
+            WRITELN(Ave DIV 10);
+          ClassTotal := ClassTotal + TotalScore
         END
     END;
-  WRITELN(OUTPUT);
-  IF NOT(Error)
+  WRITELN;
+  IF (NOT Overflow AND NOT Error)
   THEN
     BEGIN
-      WRITELN ('Class average:');
+      WRITELN('Class average:');
       ClassTotal := ClassTotal DIV (ClassSize * NumberOfScores);
       WRITELN(ClassTotal DIV 10, '.', ClassTotal MOD 10:1)
     END
-  ELSE
-    WRITELN('Œÿ»¡ ¿ ¬¬Œƒ¿!!!')
-END.  {AverageScore}
+END. {AverageScore}
